8b0212e0cfdcfb7e30285ce89a869762fcb663ba
Ritik Rawat <109600003+ritikrawatgmailcom@users.noreply.github.com>
2025-11-28 05:58:39 +0000

    object commit

---
+++ 02_Basics/object2.js
@@ -0,0 +1,11 @@
+const user={}
+user.name="Ritik"
+user.age=12
+user.isLoggedIn=false
+// console.log(user);
+
+const obj1={1:'a',2:'b',3:'c'}
+const obj2={1:'A',2:'B',3:'C'}
+const obj3={...obj1,...obj2} // it is used to combine both objects
+console.log(obj3);
+
