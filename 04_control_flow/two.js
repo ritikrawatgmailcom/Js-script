35d519832a9648c153d0c9001d0adc950d653a47
Ritik Rawat <109600003+ritikrawatgmailcom@users.noreply.github.com>
2025-12-05 08:47:42 +0000

    Dom Commit

---
+++ 04_control_flow/two.js
@@ -0,0 +1,29 @@
+// const coding=["js","ruby","java","python","cpp"]
+// coding.forEach((item)=>{
+//     console.log(item);
+    
+// })
+
+// const a=[1,2,3,4,5,6,7,8,9]
+
+// const b=a.filter((i)=>i>4) //filter work
+// console.log(b);
+
+// const a=[1,2,3,4,5,6,7,8,9]
+
+// // const b=a.filter((i)=>{i>4}) //here it not work bcz we use {} when we use it we need to add return word
+// const b=a.filter((i)=>{return i>4}) // Now it works
+// console.log(b);
+
+const a=[1,2,3,4,5,6]
+const b=a.map((i)=>i+10)
+console.log(b);
+
+
+
+
+
+
+
+
+
